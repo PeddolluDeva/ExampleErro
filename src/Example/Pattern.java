2f6da2610a2e7213a33411ceb1a077e909284f8e
PeddolluDeva <peddolludeva803@gmail.com>
2023-08-11 14:13:37 +0530

    Pattern

---
+++ src/Example/Pattern.java
@@ -0,0 +1,11 @@
+package Example;
+
+public class Pattern {
+ public static void  main(String[] args) {
+	 for(int i=1;i<=5;i++) {
+		 for(int j=5;j>=i;j--) 
+		 System.out.print("  *  ");
+		 System.out.println();
+	 }
+ }
+}
